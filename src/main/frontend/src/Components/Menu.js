a4bcb967f071f14f525be2db4d13ce32f21e48a6
amcmullen9291 <amcmullen9291@gmail.com>
2022-01-10 12:56:19 -0500

    Promise.all with useEffect working

--- src/main/frontend/src/Components/Menu.js
+++ src/main/frontend/src/Components/Menu.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
-function Menu(){
+function Menu() {
+
+let  [ Cheeses, setCheeses ] = useState(null);
+let [ Beverages , setBeverages ] = useState(null);
+
+const cheeseList = fetch('http://localhost:8080/cheeses/').then(resp => resp.json());
+const beverageList = fetch('http://localhost:8080/beverages/').then(resp => resp.json());
+
+
+useEffect(() => {
+    Promise.all([beverageList, cheeseList]).then(data => { setBeverages(data[0]); setCheeses(data[1]) })
+}, [])
+
+if((Cheeses) && Object.keys(Cheeses).length > 1){
+console.log("cheese list: ", Cheeses);
+}
+if((Beverages) && Object.keys(Beverages).length > 1){
+console.log("beverages list: ", Beverages);
+}
+
 
 return (
         <>
@@ -10,5 +30,21 @@ return (
     )
 }
 
+const mapStateToProps = (state) => {
+    return {
+      Cheeses: state.Cheeses,
+      Beverages: state.Beverages
+    }
 
-export default Menu;
+  }
+
+   const mapDispatchToProps = (dispatch) => {
+     return{
+       Cheeses: (cheeses) => { dispatch({type: 'SET_CHEESES', cheeses})},
+       Beverages: (beverages) => { dispatch({type: 'SET_BEVERAGES', beverages})},
+     }
+   }
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
